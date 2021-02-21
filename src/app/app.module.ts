3b043f49ee4d5304099d8b8e27dfa9fa49acd99b
mllaci01 <mllaci078@gmail.com>
2021-02-21 10:10:09 +0100

    project done

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { FilterPipe } from './pipe/filter.pipe';
+import { SorterPipe } from './pipe/sorter.pipe';
+import { IconComponent } from './common/icon/icon.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserDetailComponent,
+    UserListComponent,
+    FilterPipe,
+    SorterPipe,
+    IconComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
